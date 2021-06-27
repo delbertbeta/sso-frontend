241dca3d94fdbe6c037136cf29b080ecb6e676ec
delbertbeta <delbertbeta@live.com>
2021-06-27 22:46:29 +0800

    feat: add some pages

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>Index</div>
+  <h1>PrimeSSO</h1>
+  <nav>
+    <ul>
+      <li><router-link to="/login">Login</router-link></li>
+    </ul>
+  </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Index',
   components: {
-  }
-})
+  },
+});
 </script>
 
 <style>
